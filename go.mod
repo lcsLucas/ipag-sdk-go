afdb0a31726982c68a054712aa60908becd70614
Lucas Rosa <lucas.tarta@hotmail.com>
2024-07-20 23:07:03 -0300

    feat: added Logging Middleware to Customer Service

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/lcslucas/ipag-sdk-go
 
 go 1.20
+
+require (
+	github.com/go-kit/kit v0.13.0 // indirect
+	github.com/go-kit/log v0.2.0 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+)
---
+++ service/customer/log/logging.go
@@ -0,0 +1,98 @@
+package log
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+	"github.com/lcslucas/ipag-sdk-go/pkg/model"
+	"github.com/lcslucas/ipag-sdk-go/service/customer"
+)
+
+type loggingMiddleware struct {
+	next   customer.Service
+	logger log.Logger
+}
+
+func LoggingMiddleware(logger log.Logger) customer.ServiceMiddleware {
+	return func(next customer.Service) customer.Service {
+		return loggingMiddleware{next, logger}
+	}
+}
+
+func (mw loggingMiddleware) Save(ctx context.Context, customer *model.Customer) (err error) {
+	defer func(begin time.Time) {
+		logger := log.With(mw.logger, "service", "customer", "method", "save")
+		level.Info(logger).Log(
+			"parameter", fmt.Sprintf("%v", customer),
+			"error", err,
+			"ended", time.Now(),
+			"duration", fmt.Sprintf("%vms", time.Since(begin).Milliseconds()),
+		)
+	}(time.Now())
+
+	err = mw.next.Save(ctx, customer)
+	return
+}
+
+func (mw loggingMiddleware) Update(ctx context.Context, customer *model.Customer) (err error) {
+	defer func(begin time.Time) {
+		logger := log.With(mw.logger, "service", "customer", "method", "update")
+		level.Info(logger).Log(
+			"parameter", fmt.Sprintf("%v", customer),
+			"error", err,
+			"ended", time.Now(),
+			"duration", fmt.Sprintf("%vms", time.Since(begin).Milliseconds()),
+		)
+	}(time.Now())
+
+	err = mw.next.Update(ctx, customer)
+	return
+}
+
+func (mw loggingMiddleware) Find(ctx context.Context, id uint32) (c model.Customer, err error) {
+	defer func(begin time.Time) {
+		logger := log.With(mw.logger, "service", "customer", "method", "find")
+		level.Info(logger).Log(
+			"parameter", id,
+			"error", err,
+			"ended", time.Now(),
+			"duration", fmt.Sprintf("%vms", time.Since(begin).Milliseconds()),
+		)
+	}(time.Now())
+
+	c, err = mw.next.Find(ctx, id)
+	return
+}
+
+func (mw loggingMiddleware) FindAll(ctx context.Context, filters map[string]interface{}) (cs []model.Customer, err error) {
+	defer func(begin time.Time) {
+		logger := log.With(mw.logger, "service", "customer", "method", "findAll")
+		level.Info(logger).Log(
+			"parameter", filters,
+			"error", err,
+			"ended", time.Now(),
+			"duration", fmt.Sprintf("%vms", time.Since(begin).Milliseconds()),
+		)
+	}(time.Now())
+
+	cs, err = mw.next.FindAll(ctx, filters)
+	return
+}
+
+func (mw loggingMiddleware) Delete(ctx context.Context, id uint32) (err error) {
+	defer func(begin time.Time) {
+		logger := log.With(mw.logger, "service", "customer", "method", "delete")
+		level.Info(logger).Log(
+			"parameter", id,
+			"error", err,
+			"ended", time.Now(),
+			"duration", fmt.Sprintf("%vms", time.Since(begin).Milliseconds()),
+		)
+	}(time.Now())
+
+	err = mw.next.Delete(ctx, id)
+	return
+}
