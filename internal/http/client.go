5773ef5d357d5422baaf9d01ee211c9dbb4cdee0
Lucas Rosa <lucas.tarta@hotmail.com>
2024-09-01 13:33:49 -0300

    feat: Develop serialize/deserialize service api

--- internal/http/client.go
+++ internal/http/client.go
@@ -1,15 +1,7 @@
 package http
 
 import (
-	"bytes"
-	"context"
-	"encoding/base64"
-	"errors"
-	"fmt"
 	"net/http"
-	"net/url"
-
-	"github.com/lcslucas/ipag-sdk-go/config"
 )
 
 const (
@@ -19,7 +11,6 @@ const (
 
 type HTTPClient interface {
 	Do(req *http.Request) (*http.Response, error)
-	BuilderRequest(ctx context.Context, endpoint *Endpoint, config config.Config, body *bytes.Buffer) (*http.Request, error)
 }
 
 type httpClient struct {
@@ -35,27 +26,4 @@ func NewHTTPClient() *httpClient {
 
 func (httpClient *httpClient) Do(req *http.Request) (*http.Response, error) {
 	return httpClient.client.Do(req)
-}
-
-func (httpClient *httpClient) BuilderRequest(ctx context.Context, endpoint *Endpoint, config config.Config, body *bytes.Buffer) (*http.Request, error) {
-	endpointURL, err := url.Parse(fmt.Sprintf("%s/%s", config.Credentials.Environment, endpoint.URI))
-
-	if err != nil {
-		return nil, errors.New("endpoint url unprocessable in context")
-	}
-
-	request, err := http.NewRequestWithContext(ctx, string(endpoint.Method), endpointURL.String(), body)
-
-	if err != nil {
-		return nil, err
-	}
-
-	userBasicAuth := fmt.Sprintf("%s:%s", config.Credentials.ApiID, config.Credentials.ApiKey)
-
-	request.Header.Add("Content-Type", ContentTypeDefault)
-	request.Header.Add("User-Agent", UserAgentDefault)
-	request.Header.Add("x-api-version", fmt.Sprintf("%d", config.Credentials.Version))
-	request.Header.Add("Authorization", fmt.Sprintf("Basic %s", base64.StdEncoding.EncodeToString([]byte(userBasicAuth))))
-
-	return request, nil
 }
